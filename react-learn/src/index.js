a4e259818d68d9b3678ea85672041775741eefdc
Z <1162941377@qq.com>
2021-06-18 19:06:46 +0800

    组件状态

--- react-learn/src/index.js
+++ react-learn/src/index.js
@@ -1,25 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import StudentList from "./components/StudentList";
+import A from "./components/Test";
 
-const APPKEY = "demo13_1545210570249";
-const container = document.getElementById("root");
-
-// 获取所有学生的数据
-async function fetchAllStudents() {
-  const stus = await fetch(
-    "http://api.duyiedu.com/api/student/findAll?appkey=" + APPKEY
-  )
-    .then((resp) => resp.json())
-    .then((resp) => resp.data);
-  return stus;
-}
-
-// 渲染函数
-async function render() {
-  ReactDOM.render("正在加载......", container);
-  const stus = await fetchAllStudents(); // 获取学生数组
-  ReactDOM.render(<StudentList stus={stus} />, container);
-}
-
-render();
+ReactDOM.render(<A />, document.getElementById("root"));
