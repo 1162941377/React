eac00d15dc68c7a816daca0a3a41817c14b6e0dd
Z <1162941377@qq.com>
2021-06-18 13:37:50 +0800

    demo显示学生列表

---
+++ react-learn/src/components/StudentList.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import Student from "./Student";
+
+export default class StudentList extends Component {
+  render() {
+    // 约定：prop.stus，传递的是学生的数组
+    // 获取组件数组
+    // console.log(this.props.stus);
+    const students = this.props.stus.map((item) => (
+      <Student key={item.id} {...item} />
+    ));
+    return <ul>{students}</ul>;
+  }
+}
