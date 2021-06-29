ecda85db3b051740910bbdf6bb80a2814ee53302
Z <1162941377@qq.com>
2021-06-29 16:21:16 +0800

    demoHook练习

--- react-learn/src/App.js
+++ react-learn/src/App.js
@@ -1,29 +1,10 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
+import StudentContainer from "./components/StudentContainer";
 
 export default function App() {
-  const [n, setN] = useState(10);
-
-  useEffect(() => {
-    if (n === 0) {
-      return;
-    }
-
-    // 第一次渲染后，需要根据当前的n值，1秒后重新渲染
-    setTimeout(() => {
-      setN(n - 1);
-    }, 1000);
-  }, [n]);
-
   return (
     <>
-      <h1>{n}</h1>
-      <button
-        onClick={() => {
-          setN(n + 1);
-        }}
-      >
-        n + 1
-      </button>
+      <StudentContainer />
     </>
   );
 }
--- react-learn/src/services/studentData.js
+++ react-learn/src/services/studentData.js
@@ -4,9 +4,15 @@ const APPKEY = "demo13_1545210570249";
  * 获取所有学生的数据
  */
 export async function getAllStudents() {
-  return await fetch(
-    "http://api.duyiedu.com/api/student/findAll?appkey=" + APPKEY
-  )
+  return await fetch("/api/student/findAll?appkey=" + APPKEY)
     .then((res) => res.json())
     .then((res) => res.data);
+}
+
+export async function getStudents(page = 1, limit = 10) {
+  return await fetch(
+    `/api/student/findByPage?appkey=${APPKEY}&page=${page}&size=${limit}`
+  )
+    .then((resp) => resp.json())
+    .then((resp) => resp.data);
 }
