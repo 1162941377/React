d5f9e1b56f00148a2d14662e846660e6b41ae592
Z <1162941377@qq.com>
2021-07-28 08:02:53 +0800

    Hook_RefHook

--- react-learn/src/App copy.js
+++ react-learn/src/App copy.js
@@ -1,42 +1,24 @@
-import React, { PureComponent, useMemo, useState } from "react";
+import React, { useState, useRef } from "react";
 
-class Test extends PureComponent {
-  render() {
-    console.log("Test Render");
-    return (
-      <>
-        <h1>{this.props.text}</h1>
-        <button onClick={this.props.onClick}>改变文本</button>
-      </>
-    );
-  }
-}
-
-function Parent() {
-  console.log("Parent Render");
-  const [txt, setTxt] = useState(520);
-  const [n, setN] = useState(0);
-  const handleClick = useMemo(() => {
-    return () => {
-      setTxt(txt + 1);
-    };
-  }, [txt]);
-  return (
-    <>
-      {/* 函数的地址每次渲染都发生了变化，导致了子组件重新渲染，
-      若子组件是经过优化的组件，则可能导致优化失败 */}
-      <Test text={txt} onClick={handleClick} />
-      <input
-        type="number"
-        value={n}
-        onChange={(e) => {
-          setN(e.target.value);
-        }}
-      />
-    </>
-  );
-}
+window.arr = [];
 
 export default function App() {
-  return <Parent />;
+  const inpRef = useRef();
+  window.arr.push(inpRef);
+  const [n, setN] = useState(0);
+  return (
+    <>
+      <input type="text" ref={inpRef} />
+      <br />
+      <button
+        onClick={() => {
+          console.log(inpRef.current.value);
+        }}
+      >
+        得到input的值
+      </button>
+      <br />
+      <input type="number" value={n} onChange={(e) => setN(e.target.value)} />
+    </>
+  );
 }
