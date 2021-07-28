e8298332641398e4c64f227f1b2affa3564e782e
Z <1162941377@qq.com>
2021-07-28 08:48:04 +0800

    Hook_DebugValueHook

--- react-learn/src/App.js
+++ react-learn/src/App.js
@@ -1,39 +1,24 @@
-import React, { useImperativeHandle, useRef } from "react";
-
-function Test(props, ref) {
-  useImperativeHandle(
-    ref,
-    () => {
-      /**
-       * 如果不给依赖项，则每次运行函数时都会调用该方法
-       * 如果使用了依赖项，则第一次调用后，进行缓存，只有当依赖项变化时才会重新调用
-       * 相当于给 ref.current = function method() {}
-       */
-      return {
-        method() {
-          console.log("Test Component Called");
-        },
-      };
-    },
-    []
-  );
-  return <h1>Test Component</h1>;
-}
-
-const TestWrapper = React.forwardRef(Test);
+import React, { useState, useLayoutEffect, useRef } from "react";
+// import { useEffect } from "react";
 
 export default function App() {
-  const testRef = useRef();
+  const [n, setN] = useState(0);
+  const h1Ref = useRef();
+  // useEffect(() => {
+  //   h1Ref.current.innerText = Math.random().toFixed(2);
+  // });
+  useLayoutEffect(() => {
+    h1Ref.current.innerText = Math.random().toFixed(2);
+  });
   return (
     <>
-      <TestWrapper ref={testRef} />
+      <h1 ref={h1Ref}>{n}</h1>
       <button
         onClick={() => {
-          testRef.current.method();
-          console.log(testRef);
+          setN(n + 1);
         }}
       >
-        调用了Test组件的method方法
+        +
       </button>
     </>
   );
