7ecd3e20b4d4c70f41a67019b767479592de047d
Z <1162941377@qq.com>
2021-07-28 08:57:48 +0800

    Hook_DebugValueHook

--- react-learn/src/App.js
+++ react-learn/src/App.js
@@ -1,19 +1,17 @@
-import React, { useState, useLayoutEffect, useRef } from "react";
-// import { useEffect } from "react";
+import React, { useState, useEffect, useDebugValue } from "react";
+
+function useTest() {
+  const [students] = useState([]);
+  useDebugValue("学生集合");
+  return students;
+}
 
 export default function App() {
-  const [n, setN] = useState(0);
-  const h1Ref = useRef();
-  // useEffect(() => {
-  //   h1Ref.current.innerText = Math.random().toFixed(2);
-  // });
-  useLayoutEffect(() => {
-    h1Ref.current.innerText = Math.random().toFixed(2);
-  });
-  return (
-    <>
-      <h1 ref={h1Ref}>{n}</h1>
-      <button onClick={() => setN(n + 1)}>+</button>
-    </>
-  );
+  useState(0);
+  useState("zjc");
+  useEffect(() => {
+    console.log("Effect");
+  }, []);
+  useTest();
+  return <h1>Hello World!</h1>;
 }
