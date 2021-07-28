44932f2cd366ceb662b998a3caa705e19d0375f2
Z <1162941377@qq.com>
2021-07-28 09:45:55 +0800

    ReacReact_动画_Transition

--- react-learn/src/App.js
+++ react-learn/src/App.js
@@ -1,17 +1,46 @@
-import React, { useState, useEffect, useDebugValue } from "react";
+import React, { useState } from "react";
+import { Transition } from "react-transition-group";
+import "./App.css";
 
-function useTest() {
-  const [students] = useState([]);
-  useDebugValue("学生集合");
-  return students;
-}
+const duration = 2000;
 
 export default function App() {
-  useState(0);
-  useState("zjc");
-  useEffect(() => {
-    console.log("Effect");
-  }, []);
-  useTest();
-  return <h1>Hello World!</h1>;
+  const [inProp, setInProp] = useState(true);
+  return (
+    <>
+      <Transition
+        in={inProp}
+        // appear
+        // mountOnEnter={true}
+        // unmountOnExit={true}
+        // enter={false}
+        // exit={false}
+        timeout={duration}
+        addEndListener={(node, done) => {
+          node.addEventListener(
+            "transitionend",
+            () => {
+              console.log(node);
+              console.log(done);
+              // console.log("过渡结束了");
+              done();
+            },
+            { once: true }
+          );
+        }}
+      >
+        {(state) => {
+          console.log(state);
+          return <div className={state}>I'm a fade Transition!</div>;
+        }}
+      </Transition>
+      <button
+        onClick={() => {
+          setInProp(!inProp);
+        }}
+      >
+        Click to toggle!
+      </button>
+    </>
+  );
 }
