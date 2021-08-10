4ede64f6e85868968bd8955d1187f1481764923f
Z <1162941377@qq.com>
2021-08-10 08:21:16 +0800

    Router_嵌套的路由

--- react-learn/src/App copy.js
+++ react-learn/src/App copy.js
@@ -1,47 +1,52 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-// import Link from "./Link"
 
-function PageA() {
-  return <h1>A页</h1>;
-}
-
-function PageB() {
-  return <h1>B页</h1>;
-}
-
-function NavBar() {
+function User({ match }) {
+  console.log(match)
   return (
-    <div>
-      <Link
-        innerRef={(node) => {
-          console.log(node);
+    <>
+      <h1>User组件固定的区域</h1>
+      <p>
+        <Link
+          style={{
+            marginRight: 20,
+          }}
+          to={`${match.url}/update`}
+        >
+          用户信息
+        </Link>
+        <Link to={`${match.url}/pay`}>充值</Link>
+      </p>
+      <div
+        style={{
+          width: 500,
+          height: 500,
+          backgroundColor: "#008c8c",
+          border: "2px solid",
+          margin: "0 auto",
         }}
-        to="/a"
-        style={{ marginRight: 20 }}
       >
-        去a页
-      </Link>
-      <Link
-        replace
-        to={{
-          pathname: "/b",
-          hash: "#abc",
-          search: "?a=1&b=2",
-        }}
-      >
-        去b页
-      </Link>
-    </div>
+        {/* User组件变化的区域：根据地址的不同发生变化 */}
+        <Route path={`${match.url}/update`} component={UserUpdate} />
+        <Route path={`${match.url}/pay`} component={UserPay} />
+      </div>
+    </>
   );
+}
+
+function UserUpdate() {
+  return <h1>修改用户信息</h1>;
+}
+
+function UserPay() {
+  return <h1>用户充值</h1>;
 }
 
 export default function App() {
   return (
     <Router>
-      <NavBar />
-      <Route path="/a" component={PageA} />
-      <Route path="/b" component={PageB} />
+      <Route path="/u" component={User} />
+      {/* 其它组件 */}
     </Router>
   );
 }
--- react-learn/src/App.js
+++ react-learn/src/App.js
@@ -1,71 +1,47 @@
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Route,
-  NavLink,
-  Switch,
-  Redirect,
-} from "react-router-dom";
-import "./App.css";
-// import Link from "./Link"
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import routeConfig from "./routeConfig";
 
-function PageA() {
-  return <h1>A页</h1>;
-}
-
-function PageB() {
-  return <h1>B页</h1>;
-}
-
-function NavBar() {
+function User() {
   return (
-    <div>
-      <NavLink
-        activeClassName="selected"
-        exact
-        strict
-        activeStyle={{
-          background: "#ccc",
-        }}
-        innerRef={(node) => {
-          console.log(node);
-        }}
-        to="/a"
-        style={{ marginRight: 20 }}
-      >
-        去a页
-      </NavLink>
-      <NavLink
-        activeClassName="selected"
-        activeStyle={{
-          background: "#ccc",
-        }}
-        replace
-        to={{
-          pathname: "/b",
-          hash: "#abc",
-          search: "?a=1&b=2",
+    <>
+      <h1>User组件固定的区域</h1>
+      <p>
+        <Link style={{ marginRight: 20 }} to={routeConfig.user.update}>
+          用户信息
+        </Link>
+        <Link to={routeConfig.user.pay.root}>充值</Link>
+      </p>
+      <div
+        style={{
+          width: 500,
+          height: 500,
+          backgroundColor: "#008c8c",
+          border: "2px solid",
+          margin: "0 auto",
         }}
       >
-        去b页
-      </NavLink>
-
-      <NavLink to="/abc" style={{ marginLeft: 20 }}>
-        其他页
-      </NavLink>
-    </div>
+        {/* User组件变化的区域：根据地址的不同发生变化 */}
+        <Route path={routeConfig.user.update} component={UserUpdate} />
+        <Route path={routeConfig.user.pay.root} component={UserPay} />
+      </div>
+    </>
   );
+}
+
+function UserUpdate() {
+  return <h1>修改用户信息</h1>;
+}
+
+function UserPay() {
+  return <h1>用户充值</h1>;
 }
 
 export default function App() {
   return (
     <Router>
-      <NavBar />
-      <Switch>
-        <Route path="/a" component={PageA} />
-        <Route path="/b" component={PageB} />
-        <Redirect from="/abc/:id" to="/a/:id" />
-      </Switch>
+      <Route path={routeConfig.user.root} component={User} />
+      {/* 其它组件 */}
     </Router>
   );
 }
