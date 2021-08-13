4ba98f23c995036f3d71586c25f6f4cf4adad392
Z <1162941377@qq.com>
2021-08-13 10:13:09 +0800

    Router_导航守卫

--- react-learn/src/App.js
+++ react-learn/src/App.js
@@ -1,30 +1,42 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
-import Home from "./Home";
-import Personal from "./Personal";
-import Login from "./Login";
-import ProtectedRoute from "./ProtectedRoute";
+import { Route, Link } from "react-router-dom";
+import RouteGuard from "./RouteGuard";
+
+function Page1() {
+  return <h1>Page1</h1>;
+}
+
+function Page2() {
+  return <h1>Page2</h1>;
+}
 
 export default function App() {
   return (
-    <Router>
+    <RouteGuard
+      onBeforeChange={(prev, cur, action, commit, unBlock) => {
+        console.log(
+          `页面想要从${prev.pathname}跳转到${cur.pathname}，跳转方式是${action}，允许跳转`
+        );
+        commit(true);
+        unBlock(); // 取消阻塞，仅阻塞一次
+      }}
+      onChange={(prevLocation, curLocation, action, unListen) => {
+        console.log(
+          `日志：从${prevLocation.pathname}进入页面${curLocation.pathname}，进入方式是${action}`
+        );
+        unListen(); // 取消监听，仅监听一次
+      }}
+    >
       <ul>
         <li>
-          <Link to="/">首页</Link>
+          <Link to="/page1">页面1</Link>
         </li>
         <li>
-          <Link to="/login">登录页</Link>
-        </li>
-        <li>
-          <Link to="/personal">个人中心</Link>
+          <Link to="/page2">页面2</Link>
         </li>
       </ul>
-      <Switch>
-        <Route path="/login" component={Login} />
-        <ProtectedRoute path="/personal" component={Personal} />
-        {/* render和children的区别：render是匹配后才会渲染，children都会渲染 */}
-        <Route path="/" component={Home} />
-      </Switch>
-    </Router>
+      <Route path="/page1" component={Page1} />
+      <Route path="/page2" component={Page2} />
+    </RouteGuard>
   );
 }
