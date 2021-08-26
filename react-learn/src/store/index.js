93d2406ddd47036b584e7af9fcdc3693b4fad57e
Z <1162941377@qq.com>
2021-08-26 09:25:40 +0800

    Redux_applyMiddleware

--- react-learn/src/store/index.js
+++ react-learn/src/store/index.js
@@ -1,11 +1,29 @@
-import { createStore, bindActionCreators } from "../redux";
+import { createStore, bindActionCreators, applyMiddleware } from "redux";
 import reducer from "./reducer";
 import {
   createAddUserAction,
   createDeleteUserAction,
 } from "./action/usersAction";
 
-const store = createStore(reducer);
+const logger1 = (store) => (next) => (action) => {
+  console.log("中间件1");
+  console.log("旧数据", store.getState());
+  console.log("action", action);
+  next(action);
+  console.log("新数据", store.getState());
+  console.log("");
+};
+
+const logger2 = (store) => (next) => (action) => {
+  console.log("中间件2");
+  console.log("旧数据", store.getState());
+  console.log("action", action);
+  next(action);
+  console.log("新数据", store.getState());
+  console.log("");
+};
+
+const store = createStore(reducer, applyMiddleware(logger1, logger2));
 
 const actionCreators = {
   addUser: createAddUserAction,
@@ -14,11 +32,31 @@ const actionCreators = {
 
 const actions = bindActionCreators(actionCreators, store.dispatch);
 
-console.log(actions);
-
-store.subscribe(() => {
-  console.log("监听器1", store.getState());
-});
-
 actions.addUser({ id: 3, name: "zjc", age: 20 });
 actions.deleteUser(3);
+
+// function logger1(store) {
+//   return function (next) {
+//     return function (action) {
+//       console.log("中间件1");
+//       console.log("旧数据", store.getState());
+//       console.log("action", action);
+//       next(action);
+//       console.log("新数据", store.getState());
+//       console.log("");
+//     };
+//   };
+// }
+
+// function logger2(store) {
+//   return function (next) {
+//     return function (action) {
+//       console.log("中间件2");
+//       console.log("旧数据", store.getState());
+//       console.log("action", action);
+//       next(action);
+//       console.log("新数据", store.getState());
+//       console.log("");
+//     };
+//   };
+// }
