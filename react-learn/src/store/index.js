53fa68df4ac2e8efd4f1a4b036cd6787e8bd88b6
Z <1162941377@qq.com>
2021-08-27 14:06:22 +0800

    Redux_redux-thunk

--- react-learn/src/store/index.js
+++ react-learn/src/store/index.js
@@ -3,6 +3,21 @@ import reducer from "./reducer"
 import logger from "redux-logger";
 import thunk from "redux-thunk";
 
+/**
+ * thunk：
+ * 当 applyMiddleware 应用了该中间件，
+ * 允许 action 带有副作用，
+ * 返回的是一个函数，
+ * 如果该函数内部没有使用 dispatch，
+ * 那么不会进行下一个中间件的传递
+ * 也就是说 thunk 会阻碍中间件的传递，
+ * 
+ * 有三个参数：
+ * 1. dispatch
+ * 2. getState
+ * 3. extra（ thunk.withExtraArgument ）
+ */
+
 export default createStore(reducer,
   applyMiddleware(
     thunk,
