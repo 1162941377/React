468c445ad8429d7d07875658432c35c4b81584cf
Z <1162941377@qq.com>
2021-08-27 12:24:40 +0800

    Redux_redux-logger

--- react-learn/src/store/index.js
+++ react-learn/src/store/index.js
@@ -4,26 +4,9 @@ import {
   createAddUserAction,
   createDeleteUserAction,
 } from "./action/usersAction";
+import logger from "redux-logger";
 
-const logger1 = (store) => (next) => (action) => {
-  console.log("中间件1");
-  console.log("旧数据", store.getState());
-  console.log("action", action);
-  next(action);
-  console.log("新数据", store.getState());
-  console.log("");
-};
-
-const logger2 = (store) => (next) => (action) => {
-  console.log("中间件2");
-  console.log("旧数据", store.getState());
-  console.log("action", action);
-  next(action);
-  console.log("新数据", store.getState());
-  console.log("");
-};
-
-const store = createStore(reducer, applyMiddleware(logger1, logger2));
+const store = createStore(reducer, applyMiddleware(logger));
 
 const actionCreators = {
   addUser: createAddUserAction,
@@ -34,29 +17,3 @@ const actions = bindActionCreators(actionCreators, store.dispatch);
 
 actions.addUser({ id: 3, name: "zjc", age: 20 });
 actions.deleteUser(3);
-
-// function logger1(store) {
-//   return function (next) {
-//     return function (action) {
-//       console.log("中间件1");
-//       console.log("旧数据", store.getState());
-//       console.log("action", action);
-//       next(action);
-//       console.log("新数据", store.getState());
-//       console.log("");
-//     };
-//   };
-// }
-
-// function logger2(store) {
-//   return function (next) {
-//     return function (action) {
-//       console.log("中间件2");
-//       console.log("旧数据", store.getState());
-//       console.log("action", action);
-//       next(action);
-//       console.log("新数据", store.getState());
-//       console.log("");
-//     };
-//   };
-// }
