83a54ae4368856ff9a90105ef1bb0ebe606b7796
Z <1162941377@qq.com>
2021-09-08 09:28:23 +0800

    Redux_thunk管理学生数据

---
+++ react-learn/src/store/action/student/searchResult.js
@@ -0,0 +1,50 @@
+import { searchStudents } from "../../../services/student";
+
+/**
+ * 对学生查询结果改变的action的类型
+ */
+export const actionTypes = {
+  setStudentsAndTotal: Symbol("set-student-and-toal"),
+  setIsLoading: Symbol("set-is-loading"),
+};
+
+/**
+ * 根据新的查询条件，产生一个action
+ * @param {Array} arr
+ * @param {Number} total
+ */
+export function setStudentsAndTotal(arr, total) {
+  return {
+    type: actionTypes.setStudentsAndTotal,
+    payload: {
+      datas: arr,
+      total,
+    },
+  };
+}
+
+/**
+ * 得到一个设置是否正在加载的action
+ * @param {Boolean} isLoading
+ * @returns
+ */
+export function setIsLoading(isLoading) {
+  return {
+    type: actionTypes.setIsLoading,
+    payload: isLoading,
+  };
+}
+
+/**
+ * 根据当前仓库中的查询条件，查询学生
+ * @returns
+ */
+export function fetchStudents() {
+  return async function (dispatch, getState) {
+    dispatch(setIsLoading(true));
+    const condition = getState().students.condition;
+    const resp = await searchStudents(condition);
+    dispatch(setStudentsAndTotal(resp, resp.cont));
+    dispatch(setIsLoading(false));
+  };
+}
