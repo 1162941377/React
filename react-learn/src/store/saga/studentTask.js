4a5fa5af3b3b30af0238054f68ff75d884347ef2
Z <1162941377@qq.com>
2021-11-07 13:16:22 +0800

    React_redux【demo】查询学生

--- react-learn/src/store/saga/studentTask.js
+++ react-learn/src/store/saga/studentTask.js
@@ -3,29 +3,8 @@ import {
   setIsLoading,
   setStudentsAndTotal,
 } from "../action/student/searchResult";
-import { takeEvery, put, cps, select } from "redux-saga/effects";
-
-/**
- * 回调模式的异步
- * @param {*} callback
- */
-function mockStudents(condition, callback) {
-  console.log("mockStudents", condition);
-  setTimeout(() => {
-    if (Math.random() > 0.5) {
-      //nodejs风格
-      callback(null, {
-        cont: 78,
-        datas: [
-          { id: 1, name: "abc" },
-          { id: 2, name: "bcd" },
-        ],
-      });
-    } else {
-      callback(new Error("出错了！！！1"), null);
-    }
-  }, 1000);
-}
+import { takeEvery, put, call, select } from "redux-saga/effects";
+import { searchStudents } from "../../services/student";
 
 function* fetchStudentsSaga() {
   //设置为正在加载中
@@ -33,7 +12,7 @@ function* fetchStudentsSaga() {
   const condition = yield select((state) => state.students.condition);
   //使用call指令，按照当前仓库中的条件
   try {
-    const resp = yield cps(mockStudents, condition);
+    const resp = yield call(searchStudents, condition);
     yield put(setStudentsAndTotal(resp.datas, resp.cont));
   } catch (err) {
     console.log(err.message);
@@ -43,6 +22,5 @@ function* fetchStudentsSaga() {
 }
 
 export default function* studentTask() {
-  yield takeEvery(fetchStudents.toString(), fetchStudentsSaga);
-  console.log("正在监听 fetchStudents");
+  yield takeEvery(fetchStudents, fetchStudentsSaga);
 }
